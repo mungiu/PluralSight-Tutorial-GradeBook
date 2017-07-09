b6df10c9befe57ff6f2bfea9ac923fbd182ab8e8
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-07-09 15:54:06 +0200

    Added all code files via upload
    This is the application in its final form after i have finished 1/3 of the Basics Tutorial on PluralSight

---
+++ GradeBook.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSGrades
+{
+    public class GradeBook : GradeTracker
+    {
+        public GradeBook()
+        {
+            _name = "Empty";
+            grades = new List<float>();
+        }
+
+        public bool ThrowAwayLowest { get; set; }
+
+        //we use "virtual" so when C# compiler observes we are invoking
+        //ComputeStatistics(); through a variable types as "GradeBook"
+        //it will no longer use the type of "variable" to figure out
+        //which method to call, insted it will use the type of "object"
+        //the type it sees at "runtime"
+        //so when it sees that GradeBook is utimatelly a ThrowAwayGrade 
+        //it will check if the "ThrowAwayGradeBook" overrides "ComputeStatistics();"
+        public override GradeStatistics ComputeStatistics()
+        {
+            //Indicating where the debug takes place (pop-up writeline)
+            Console.WriteLine("GradeBook::ComputeStatistics");
+
+            GradeStatistics stats = new GradeStatistics();
+            
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
+        }
+
+        
+        // Creating method "WriteGrades"
+        // "destination" is declared as a "TextWriter"
+        // "destination" - writes grades from an "array" to a destination
+        public override void WriteGrades(TextWriter destination)
+        {
+            for (int i = grades.Count; i > 0; i--)
+            {
+                destination.WriteLine(grades[i-1]);
+            }
+        }
+
+        
+        public override void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        //Implementing GetEnumerator 
+        public override IEnumerator GetEnumerator()
+        {
+            return grades.GetEnumerator();
+        }
+        protected List<float> grades;
+        
+    }
+}
---
+++ GradeTracker.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSGrades
+{
+    //This is an "abstract" class
+    //This means we can work with all the "methods" it has inside
+    //By making sure that all other classes will inherit from this abstract class
+    //That way classes that inherit from it can use all the methods inside of it
+    public abstract class GradeTracker : IGradeTracker
+    {
+        //The below three lines of code satisfy the constraints of the "Interface"
+        public abstract void AddGrade(float grade);
+        public abstract GradeStatistics ComputeStatistics();
+        public abstract void WriteGrades(TextWriter destination);
+        //Because IGradeTacker inherits from IEnumerator, therefore IEnumerator method
+        //must be listed in this abstract class (found by pressing F12 on IEnumerator)
+        public abstract IEnumerator GetEnumerator();
+        //By adding "get/set", method = "property type" and != "field type"
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                while (String.IsNullOrEmpty(value))
+                {
+                    GetBookName();
+                    value = Console.ReadLine();
+                }
+
+                if (_name != value)
+                {
+                    NameChangedEventArgs args = new NameChangedEventArgs();
+                    args.ExistingName = _name;
+                    args.NewName = value;
+
+                    NameChanged(this, args);
+                }
+            }
+        }
+        private static void GetBookName()
+        {
+            try
+            {
+                throw new ArgumentException("GradeBook name cannot be null/empty.");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+        //Creating "public field" using "public event" type
+        //"public field" can now be assigned smth.
+        //_name = "private field" for holding original string value (Name)
+        public event NameChangedDelegate NameChanged;
+        protected string _name;
+    }
+}
